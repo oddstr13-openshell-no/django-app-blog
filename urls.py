a3e94fdafc5b748197e8cd92a909a5ee49b991ae
Odd Stråbø <oddstr13@openshell.no>
2024-01-14 18:34:25 +0100

    Update to Django 1.10

--- urls.py
+++ urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$',                  views.index),
-    url(r'^post/(?P<id>.+)/$',  views.post),
-    url(r'^tag/(?P<tag>.+)/$',  views.by_tag),
+    url(r'^post/(?P<id>.+)/$',  views.post, name='blog-post'),
+    url(r'^tag/(?P<tag>.+)/$',  views.by_tag, name='blog-by-tag'),
 ]
